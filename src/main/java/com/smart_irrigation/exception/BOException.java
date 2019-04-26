85380f8633e73d01b845509a4f787f69061a0cff
Pietro Marinho <pietro.marinho@timeitn.com.br>
2019-04-26 17:05:23 -0300

    remove old service

--- src/main/java/com/smart_irrigation/exception/BOException.java
+++
@@ -1,8 +0,0 @@
-package com.smart_irrigation.exception;
-
-public class BOException extends RuntimeException {
-    
-	private static final long serialVersionUID = 1L;
-	public BOException(String message){super(message);}
-    public BOException(String message, Throwable throwable){super(message,throwable);}
-}
--- src/main/java/com/smart_irrigation/persistence/repositories/PlantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smart_irrigation.persistence.repositories;
-
-import com.smart_irrigation.persistence.model.Plant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlantRepository extends JpaRepository<Plant, Long>{
-}
